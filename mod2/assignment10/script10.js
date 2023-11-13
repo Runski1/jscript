8c6bb3eded4fc1d5cf3e498fb0b54ab921d3106a
Matias Ruonala <142801440+Runski1@users.noreply.github.com>
2023-11-13 16:59:19 +0200

    mod2done

--- mod2/assignment10/script10.js
+++ mod2/assignment10/script10.js
@@ -4,12 +4,23 @@ const candidates = [];
 for (let i = 1; i <=  numOfCandidates; i++) {
     candidates.push({'name': prompt("Name for candidate " + i), 'votes': 0});
 }
-console.log(candidates);
+
 const numOfVoters = Number(prompt("How many voters?"));
 let votedCandidate = "";
 let promptString = "";
+
 for (let i = 1; i <= numOfVoters; i++) {
     promptString = `Voter ${i}, who do you vote?` // annoying little ticks needed
-    votedCandidate = prompt(promptString)
+    votedCandidate = prompt(promptString);
 
+    for (let candidate of candidates) {
+        if (candidate.name == votedCandidate) {
+            candidate.votes += 1;
+        }
+    }
 }
+function sortByVotes(a, b) {
+    return b.votes - a.votes;
+}
+candidates.sort(sortByVotes)
+console.log(candidates)
