11fc599f9c04072f03b479dc595e318106345f41
Matias Ruonala <142801440+Runski1@users.noreply.github.com>
2023-11-20 12:34:08 +0200

    maybe broke

--- mod4/assignment5/script5.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-async function getJoke() {
-    const response = await fetch('https://api.chucknorris.io/jokes/random');
-    const jokeData = await response.json();
-    console.log(jokeData.value)
-}
-getJoke();
